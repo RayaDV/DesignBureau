d7fde3da7c2ac74ebe8e7fa9dc8bef1393212720
RayaDV <raya.d.dimitrova@gmail.com>
2024-03-24 18:04:03 +0200

    Common Repository added.

---
+++ DesignBureau.Infrastructure/Common/Repository.cs
@@ -0,0 +1,35 @@
+﻿using DesignBureau.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignBureau.Infrastructure.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+
+        public Repository(DesignBureauDbContext context)
+        {
+            this.context = context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class // make method which return specific table
+        {
+            return context.Set<T>();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>().AsNoTracking();
+        }
+    }
+}
