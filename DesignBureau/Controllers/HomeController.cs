2d132933cbb5f205a5997494581a6b429ae27134
RayaDV <raya.d.dimitrova@gmail.com>
2024-03-29 00:37:03 +0200

    Home controller methods implemented

--- DesignBureau/Controllers/HomeController.cs
+++ DesignBureau/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using DesignBureau.Core.Models.Home;
+﻿using DesignBureau.Core.Contracts;
+using DesignBureau.Core.Models.Home;
 using DesignBureau.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,10 +8,17 @@ namespace DesignBureau.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IProjectService projectService;
+
+        public HomeController(IProjectService projectService)
+        {
+            this.projectService = projectService;
+        }
 
         public async Task<IActionResult> Index()
         {
-            var model = new IndexViewModel();
+            var model = await projectService.AllProjectsFromBack();
+
             return View(model);
         }
 
